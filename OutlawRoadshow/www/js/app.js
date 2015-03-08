e651cdbb2464842ac042f2e28b4de82f33cceda0
rtsbeacon <rtsbeacon@gmail.com>
2015-03-08 13:38:21 -0400

    this might fuck up everything

--- OutlawRoadshow/www/js/app.js
+++ OutlawRoadshow/www/js/app.js
@@ -22,8 +22,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 })
 
 
-
-
 //this is code for the slide menu
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider

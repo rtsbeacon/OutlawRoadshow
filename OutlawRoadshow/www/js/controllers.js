65d1ac8283a6800be77c8652605d80ac26b901ed
rtsbeacon <rtsbeacon@gmail.com>
2015-03-09 23:18:04 -0400

    trying to fix links

--- OutlawRoadshow/www/js/controllers.js
+++ OutlawRoadshow/www/js/controllers.js
@@ -27,6 +27,18 @@ angular.module('starter.controllers', [])
      });
 }])
 
+
+.controller('InAppBrowserCtrl', function($scope, $cordovaInAppBrowser) {
+  $scope.openjsonurlinnewbrowser = function(url) {
+    $cordovaInAppBrowser
+     .open(url, '_blank')
+     .then(function(event) {
+       // success
+     }, function(event) {
+       // error
+    });
+  };
+)};
 //.controller('ChatsCtrl', function($scope, Chats) {
 //  $scope.chats = Chats.all();
 //  $scope.remove = function(chat) {

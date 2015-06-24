d140fcd9a5cda3bcbde9c7dba80ca832abb7210a
rtsbeacon <rtsbeacon@gmail.com>
2015-06-23 21:04:34 -0400

    changing controller files

--- OutlawRoadshow/www/js/controllers.js
+++ OutlawRoadshow/www/js/controllers.js
@@ -1,13 +1,38 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $rootScope, $ionicUser, $ionicPush) {
-  
+
+  // Handles incoming device tokens
+  $rootScope.$on('$cordovaPush:tokenReceived', function(event, data) {
+    alert("Successfully registered token " + data.token);
+    console.log('Ionic Push: Got token ', data.token, data.platform);
+    $scope.token = data.token;
+  });
+
   // Identifies a user with the Ionic User service
   $scope.identifyUser = function() {
-    // Your identify code from before
+    console.log('Ionic User: Identifying with Ionic User service');
+
+    var user = $ionicUser.get();
+    if(!user.user_id) {
+      // Set your user_id here, or generate a random one.
+      user.user_id = $ionicUser.generateGUID()
+    };
+
+    // Add some metadata to your user object.
+    angular.extend(user, {
+      name: 'Ionitron',
+      bio: 'I come from planet Ion'
+    });
+
+    // Identify your user with the Ionic User Service
+    $ionicUser.identify(user).then(function(){
+      $scope.identified = true;
+      alert('Identified user ' + user.name + '\n ID ' + user.user_id);
+    });
   };
-  
-  // Registers a device for push notifications and stores its token
+
+  // Registers a device for push notifications 
   $scope.pushRegister = function() {
     console.log('Ionic Push: Registering user');
 

2c95c08bd82784584ddbdf458cd6b39f7b59e186
Lindsay Dolph <rtsbeacon@gmail.com>
2015-09-15 22:37:42 -0400

    cleaned up app file

--- OutlawRoadshow/www/js/app.js
+++ OutlawRoadshow/www/js/app.js
@@ -1,10 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', [
   'ionic','ionic.service.core','ngCordova','ionic.service.push',
   
@@ -50,13 +43,6 @@ angular.module('starter', [
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
-
-        // user_id: device.uuid  Identify your user with the Ionic User Service
-   // $ionicUser.identify(user).then(function(){
-    //  alert("user identified");
-    //  console.log('user identified');
-    //   });
-  
                        
     $ionicPush.register({
             canShowAlert: true, //Can pushes show an alert on your screen?
@@ -366,99 +352,3 @@ angular.module('starter', [
   $urlRouterProvider.otherwise('/app/schedule');
 });
 
-
-
-// this is code for sticky dividers 
-
-//.directive('sticky', function($ionicScrollDelegate) {
-//	var options,
-//		defaults = {
-//		classes: {
-//			animated: 'item-animated',
-//			container: 'item-wrapper',
-//			hidden: 'item-hidden',
-//			stationaryHeader: 'item item-divider'
-//		},
-//		selectors: {
-//			groupContainer: 'item-container',
-//			groupHeader: 'item-divider',
-//			stationaryHeader: 'div'
-//		}
-//	};
-//	return {
-//		restrict: 'A',
-//		link: function(scope, element, attrs, ctrl) {
-//
-//				var items = [],
-//					options = angular.extend(defaults, attrs),
-//					$element = angular.element(element),
-//					$fakeHeader = angular.element('<div class="' + options.classes.stationaryHeader + '"/>'),
-//					$groupContainer = angular.element($element[0].getElementsByClassName(options.selectors.groupContainer));
-//
-//				$element.addClass('list-sticky');
-//
-//				angular.element($element[0].getElementsByClassName('list')).addClass(options.classes.container);
-//
-//				$element.prepend($fakeHeader);
-//
-//				angular.forEach($groupContainer, function(elem, index) {
-//
-//					var $tmp_list = $groupContainer.eq(index);
-//						$tmp_header = angular.element($tmp_list[0].getElementsByClassName(options.selectors.groupHeader)).eq(0),
-//						$tmp_listHeight = $tmp_list.prop('offsetHeight'),
-//						$tmp_listOffset = $tmp_list[0].getBoundingClientRect().top ;
-//
-//					items.push({
-//						'list': $tmp_list,
-//						'header': $tmp_header,
-//						'listHeight': $tmp_listHeight,
-//						'headerText': $tmp_header.text(),
-//						'headerHeight': $tmp_header.prop('offsetHeight'),
-//						'listOffset': $tmp_listOffset,
-//						'listBottom': $tmp_listHeight + $tmp_listOffset
-//					});
-//				});
-
-//				$fakeHeader.text(items[0].headerText);
-
-//				scope.checkPosition = function() {
-//					var i = 0,
-//						topElement, offscreenElement, topElementBottom,
-//						currentTop = $ionicScrollDelegate.$getByHandle('scrollHandle').getScrollPosition().top;
-//
-//					while ((items[i].listOffset - currentTop) <= 0) {
-//						topElement = items[i];
-//						topElementBottom = -(topElement.listBottom - currentTop);
-//
-//						if (topElementBottom < -topElement.headerHeight) {
-//							offscreenElement = topElement;
-//						}
-
-//						i++;
-
-//						if (i >= items.length) {
-//							break;
-//						}
-//					}
-
-// it has been suggested that if you change if (topElement) to if (topElementBottom) this will work with ng_repeat but it didnt seem to make a difference for me.. also was not sure if they meant the one on line 210 or 217
-//					if (topElement) {
-
-//						if (topElementBottom < 0 && topElementBottom > -topElement.headerHeight) {
-//							$fakeHeader.addClass(options.classes.hidden);
-//							angular.element(topElement.list).addClass(options.classes.animated);
-//						} else {
-//							$fakeHeader.removeClass(options.classes.hidden);
-//							if (topElement) {
-//								angular.element(topElement.list).removeClass(options.classes.animated);
-//							}
-//						}
-//						$fakeHeader.text(topElement.headerText);
-//					} else {
-  //            $fakeHeader.addClass(options.classes.hidden);
-    //        }
-//				}
-//		}
-//
-//	}
-//});
